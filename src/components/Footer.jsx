3fa4350a035c83e38556f03e5fa02254b5a676c3
Gustavo Bordinasso <gustavo@email.com>
2025-10-09 17:33:06 -0300

    Atualizacao dos cursos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>©2025 TechCursos</p>
+      <p style={{color: "black"}}>©2025 TechCursos</p>
       <div className="footer-links">
-        <a href="#">Ajuda e Suporte</a>
-        <a href="#">Configurações de cookie</a>
-        <a href="#">Política de privacidade</a>
+        <a href="#" style={{color: "black"}}>Ajuda e Suporte</a>
+        <a href="#" style={{color: "black"}}>Configurações de cookie</a>
+        <a href="#" style={{color: "black"}}>Política de privacidade</a>
       </div>
-      <p>🌐 Português</p>
+      <p style={{color: "black"}}>🌐 Português</p>
     </footer>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cadastro from "./components/cadastro/cadastro.jsx";
 import Login from "./components/Login/Login.jsx";
 import ErrorPage from "./Error/index.jsx";
+import ProgramacaoWeb from "./cursos/pw/pw.jsx";
+import LinguagensProgramacao from "./cursos/lp/lp.jsx";
+import BancoDeDados from "./cursos/bd/bd.jsx";
+import Informatica from "./cursos/info/info.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-  <Route path="/cadastro" element={<Cadastro />} />
-  <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<ErrorPage />} />
+        <Route path="/programacao-web" element={<ProgramacaoWeb />} />
+        <Route path="/linguagens-de-programacao" element={<LinguagensProgramacao />} />
+        <Route path="/banco-de-dados" element={<BancoDeDados />} />
+        <Route path="/informatica" element={<Informatica />} />
       </Routes>
     </Router>
   </React.StrictMode>
